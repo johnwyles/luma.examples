fb775db5e5f99591ba1aea991c2fd75172d63521
John Wyles <john@johnwyles.com>
2021-05-13 16:28:31 -0700

    Adding comments and changing the IP address to use the python netifaces module

--- examples/carousel_client_appdaemon.py
+++ examples/carousel_client_appdaemon.py
@@ -1,12 +1,13 @@
 import appdaemon.plugins.hass.hassapi as hass
 import json
 import logging
+import netifaces
 import psutil
 import socket
 import time
 
-HOST = "192.168.1.9"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+SERVER_HOST = "192.168.1.9"  # The server's hostname or IP address
+SERVER_PORT = 65432  # The port used by the server
 SLEEP_INTERVAL = 0.5
 
 
@@ -16,47 +17,56 @@ class Carousel(hass.Hass):
         logging.basicConfig(level=logging.INFO)
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            logging.info('Connecting: {} : {}'.format(HOST, PORT))
-            s.connect((HOST, PORT))
+            logging.info('Connecting: {} : {}'.format(SERVER_HOST, SERVER_PORT))
+            s.connect((SERVER_HOST, SERVER_PORT))
 
             while True:
                 logging.info("Getting system data...")
+
+                # CPU
                 cpu_percent = psutil.cpu_percent(interval=None, percpu=False)
                 cpu_percent_each = psutil.cpu_percent(interval=None, percpu=True)
 
+                # Memory
                 mem = psutil.virtual_memory()
                 swap = psutil.swap_memory()
                 mem_used_pct = (mem.total - mem.available) * 100.0 / mem.total
 
+                # Disk
                 df = psutil.disk_usage("/")
 
+                # Network
                 address = "Unknown"
                 bytes_recv = 0
                 bytes_sent = 0
                 counters = []
                 interface_name = "unknown"
+                default_gw = netifaces.gateways()['default'][netifaces.AF_INET]
+                ip_address = netifaces.ifaddresses(default_gw[1])[netifaces.AF_INET][0]['addr']
                 if "eth0" in psutil.net_if_addrs():
                     interface_name = "eth0"
-                    address = psutil.net_if_addrs()["eth0"][0].address
+                    interface_address = psutil.net_if_addrs()["eth0"][0].address
                     bytes_recv = psutil.net_io_counters(pernic=True)["eth0"].bytes_recv
                     bytes_sent = psutil.net_io_counters(pernic=True)["eth0"].bytes_sent
                 elif "wlan0" in psutil.net_if_addrs():
                     interface_name = "wlan0"
-                    address = psutil.net_if_addrs()["wlan0"][0].address
+                    interface_address = psutil.net_if_addrs()["wlan0"][0].address
                     bytes_recv = psutil.net_io_counters(pernic=True)["wlan0"].bytes_recv
                     bytes_sent = psutil.net_io_counters(pernic=True)["wlan0"].bytes_sent
                 elif "en0" in psutil.net_if_addrs():
                     interface_name = "en0"
-                    address = psutil.net_if_addrs()["en0"][0].address
+                    interface_address = psutil.net_if_addrs()["en0"][0].address
                     bytes_recv = psutil.net_io_counters(pernic=True)["en0"].bytes_recv
                     bytes_sent = psutil.net_io_counters(pernic=True)["en0"].bytes_sent
 
+                # Battery
                 battery_pct = ""
                 try:
                     battery_pct = psutil.sensors_battery().percent
                 except:
                     battery_pct = "100"
 
+                # Build the payload
                 send_data = {
                     "cpu": {
                         "cpu_percent": cpu_percent,
@@ -73,8 +83,9 @@ class Carousel(hass.Hass):
                         "mem_used_pct": mem_used_pct,
                     },
                     "network": {
+                        "ip_address": ip_address,
                         "interface_name": interface_name,
-                        "address": address,
+                        "interface_address": interface_address,
                         "bytes_recv": bytes_recv,
                         "bytes_sent": bytes_sent
                     },
@@ -83,18 +94,20 @@ class Carousel(hass.Hass):
                     }
                 }
 
+                # Convert to JSON and send
                 data = json.dumps(send_data)
-
                 logging.info('Sending JSON system data: {}'.format(data))
                 s.sendall(data.encode('utf-8'))
                 logging.info('Waiting for OK...')
 
+                # Acknowledged
                 data = s.recv(1024).decode('utf-8')
                 if data != 'OK':
                     logging.error('Not OK.')
                     break
                 logging.info('Received OK.')
 
+                # Wait
                 time.sleep(SLEEP_INTERVAL)
 
         print("Received", repr(data))
--- examples/carousel_server.py
+++ examples/carousel_server.py
@@ -29,25 +29,26 @@ def render_disk_memory_battery(draw, width, height):
     logging.info('Rendering Disk and Memory...')
     margin = 3
 
+    # Disk
     title_text(draw, margin, width, text="Disk")
     draw.text((margin, 15), text="Used:", font=tiny_font, fill="white")
     draw.text((margin, 25), text="Free:", font=tiny_font, fill="white")
     draw.text((margin, 35), text="Total:", font=tiny_font, fill="white")
-
     right_text(draw, 15, width, margin, text="{0:0.1f}%".format(input_data["disk"]["used_pct"]))
     right_text(draw, 25, width, margin, text=bytes2human(input_data["disk"]["free"], "{0:0.0f}"))
     right_text(draw, 35, width, margin, text=bytes2human(input_data["disk"]["total"], "{0:0.0f}"))
 
+    # Memory
     x = (width - draw.textsize("Memory")[0]) / 2
     draw.text((x, 45), text="Memory", fill="yellow")
     draw.text((margin, 57), text="Used:", font=tiny_font, fill="white")
     draw.text((margin, 67), text="Phys:", font=tiny_font, fill="white")
     draw.text((margin, 77), text="Swap:", font=tiny_font, fill="white")
-
     right_text(draw, 57, width, margin, text="{0:0.1f}%".format(input_data["memory"]["mem_used_pct"]))
     right_text(draw, 67, width, margin, text=bytes2human(input_data["memory"]["mem_used"]))
     right_text(draw, 77, width, margin, text=bytes2human(input_data["memory"]["swap_used"]))
 
+    # Battery
     draw.text((x, 90), text="Battery", fill="yellow")
     battery_pct = input_data["battery"]["battery_pct"]
     draw.text((margin, 105), text="{} %".format(battery_pct), font=tiny_font, fill="white")
@@ -73,22 +74,24 @@ def render_network_cpu(draw, width, height):
     top_margin = 3
     bottom_margin = 3
 
+    # Network
     title_text(draw, top_margin, width, "Net: {}".format(input_data["network"]["interface_name"]))
-    address = input_data["network"]["address"]
-
-    draw.text((margin, 15), text=address, font=tiny_font, fill="white")
+    # interface_address = input_data["network"]["interface_address"]
+    ip_address = input_data["network"]["ip_address"]
+    # draw.text((margin, 15), text=interface_address, font=tiny_font, fill="white")
+    draw.text((margin, 15), text=ip_address, font=tiny_font, fill="white")
     draw.text((margin, 25), text="Rx:", font=tiny_font, fill="white")
     draw.text((margin, 35), text="Tx:", font=tiny_font, fill="white")
     right_text(draw, 25, width, margin, text=bytes2human(input_data["network"]["bytes_recv"]))
     right_text(draw, 35, width, margin, text=bytes2human(input_data["network"]["bytes_sent"]))
 
+    # CPU
     x = (width - draw.textsize("CPU Load")[0]) / 2
     draw.text((x, 45), text="CPU Load", fill="yellow")
     bar_height = (height - 15 - top_margin - bottom_margin) / 1.95
     width_cpu = width / len(input_data["cpu"]["cpu_percent_each"])
     bar_width = 0.5 * width_cpu
     bar_margin = (width_cpu - bar_width) / 1.95
-
     cpu_percent_string = "{}%".format(str(input_data["cpu"]["cpu_percent"]))
     x = (width - draw.textsize(cpu_percent_string)[0]) / 1.7
     draw.text((x, 57), text=cpu_percent_string, font=tiny_font, fill="white")
@@ -99,7 +102,6 @@ def render_network_cpu(draw, width, height):
         vertical_bar(draw,
                      x, y2 - bar_height - 1,
                      x + bar_width, y2, y2 - cpu_height)
-
         x += width_cpu
 
 
