4a8b3de7cae890937f975903bcc1d2610fde9198
John Wyles <john@johnwyles.com>
2021-05-23 14:21:19 -0700

    Updating server launch script

--- carousel_server_sd1351.sh
+++ carousel_server_sd1351.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 examples/carousel_server.py -f conf/ssd1351.conf --framebuffer=full_frame
+python3 examples/carousel_server_sd1351.py -f conf/ssd1351.conf --framebuffer=full_frame
